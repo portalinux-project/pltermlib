6d1dd627bc79d4dd17484a8f30c0cf9216d48da0
pocketlinux32 <pocketlinux32@gmail.com>
2024-05-09 01:46:08 -0500

    plterm compiles

--- src/plterm-basic.c
+++ src/plterm-basic.c
@@ -42,17 +42,17 @@ pltermsc_t plTermTILeftRight(plterm_t* termStruct, plptr_t buffer, size_t* bufSe
 	plTermGetAttrib(&terminalSize, PLTERM_SIZE, termStruct);
 
 	if(*bufSeekbyte > 0 && inputKey.bytes[0] == PLTERM_KEY_LEFT){
-		if(((plchar_t*)buffer.pointer)[bufSeekbyte - 1].bytes[0] == '\t')
-			movementUnits = tabWidth
+		if(((plchar_t*)buffer.pointer)[*bufSeekbyte - 1].bytes[0] == '\t')
+			movementUnits = tabWidth;
 
-		if(currentPos.x - movementUnits == 0 || currentPos.x - movementUnits > currentPox.x)
+		if(currentPos.x - movementUnits == 0 || currentPos.x - movementUnits > currentPos.x)
 			plTermMove(termStruct, terminalSize.x, currentPos.y - 1);
 		else
 			plTermRelMove(termStruct, -movementUnits, 0);
 
 		(*bufSeekbyte)--;
 	}else if(*bufSeekbyte + 1 < buffer.size && inputKey.bytes[0] == PLTERM_KEY_RIGHT){
-		if(((plchar_t*)buffer.pointer)[bufSeekbyte].bytes[0] == '\t')
+		if(((plchar_t*)buffer.pointer)[*bufSeekbyte].bytes[0] == '\t')
 			movementUnits = tabWidth;
 
 		if(currentPos.x + movementUnits >= terminalSize.x)
@@ -69,9 +69,9 @@ pltermsc_t plTermTILeftRight(plterm_t* termStruct, plptr_t buffer, size_t* bufSe
 
 pltermsc_t plTermTIEditing(plterm_t* termStruct, plptr_t* buffer, size_t* bufSeekbyte, plchar_t inputKey, uint16_t tabWidth){
 	if(*bufSeekbyte + 1 < buffer->size)
-		memmove(buffer->pointer + *bufSeekbyte + 1, buffer->*buffer + *bufSeekbyte, buffer->size - *bufSeekbyte)
+		memmove(buffer->pointer + *bufSeekbyte + 1, buffer->pointer + *bufSeekbyte, buffer->size - *bufSeekbyte);
 
-	buffer->pointer[*bufSeekbyte] = inputKey;
+	((plchar_t*)buffer->pointer)[*bufSeekbyte] = inputKey;
 	buffer->size++;
 
 	return plTermTILeftRight(termStruct, *buffer, bufSeekbyte, inputKey, tabWidth);
@@ -87,56 +87,5 @@ void plTermReadline(plterm_t* termStruct, plptr_t* buffer){
 }
 
 void plTermTextbox(plterm_t* termStruct, plptr_t* buffer){
-	pltermsc_t startPos, currentPos, terminalSize;
-	size_t bufferSizeLimit = buffer->size;
-	size_t bufferSeekbyte = 0;
-	buffer->size = strlen(buffer->pointer);
-	plTermGetAttrib(&startPos, PLTERM_POS, termStruct);
-	plTermGetAttrib(&terminalSize, PLTERM_SIZE, termStruct);
-	currentPos = startPos;
-	if(buffer->size > 0)
-		plTermPrint(termStruct, plRTStrFromPLPtr(*buffer, NULL, false, false));
 
-	bool directionalKey = false;
-	plchar_t inputKey = plTermGetInput(termStruct);
-	switch(inputKey.bytes[0]){
-		case PLTERM_KEY_UP:
-			if(currentPos.y == startPos.y){
-				if(currentPos.x != startPos.x){
-					plTermMove(termStruct, startPos.x, startPos.y);
-					currentPos = startPos;
-					bufferSeekbyte = 0;
-				}
-			}else{
-				plTermRelMove(termStruct, 0, -1);
-				currentPos.y--;
-				bufferSeekbyte -= terminalSize.x;
-			}
-			directionalKey = true;
-			break;
-		case PLTERM_KEY_DOWN:
-			if((bufferSeekbyte + 1) + terminalSize.x < buffer->size){
-				plTermRelMove(termStruct, 0, 1);
-				currentPos.y++;
-				bufferSeekbyte += terminalSize.x;
-			}else if(bufferSeekbyte + 1 != buffer->size){
-				if(currentPos.x + (buffer->size - (bufferSeekbyte + 1)) < terminalSize.x)
-					plTermRelMove(termStruct, buffer->size - (bufferSeekbyte + 1), 0);
-				else
-					plTermMove(termStruct, buffer->size - ((currentPos.y) * terminalSize.x), currentPos.y + 1);
-
-				plTermGetAttrib(&currentPos, PLTERM_POS, termStruct);
-				bufferSeekbyte = buffer->size - 1;
-			}
-			directionalKey = true;
-			break;
-		case PLTERM_KEY_LEFT:
-		case PLTERM_KEY_RIGHT:
-			currentPos = plTermTILeftRight(termStruct, buffer->size, &bufferSeekbyte, inputKey);
-		case PLTERM_KEY_BACKSPACE:
-			directionalKey = true;
-	}
-
-	if(buffer->size < bufferSizeLimit && !directionalKey)
-		currentPos = plTermTIEditing(termStruct, buffer, &bufferSeekbyte, inputKey);
 }
